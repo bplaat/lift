28e63e952b165dc67ab9332f1abb2a0b5328e84c
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-09-26 14:29:33 +0200

    Update code

--- bastiaan_etage/bastiaan_etage.ino
+++ bastiaan_etage/bastiaan_etage.ino
@@ -5,11 +5,7 @@
 
 #include "digit_display.h"
 
-#define BUTTON_PIN 9
-#define LED_PIN 10
-
 uint8_t counter = 0;
-bool button_pressed = false;
 
 DigitDisplay *digit_display;
 uint8_t digit_display_pins[7] = { 2, 3, 4, 5, 6, 7, 8 };
@@ -17,30 +13,15 @@ uint8_t digit_display_pins[7] = { 2, 3, 4, 5, 6, 7, 8 };
 void setup() {
     Serial.begin(9600);
     Serial.println("Bastiaan's Etage");
-
     digit_display = digit_display_new(digit_display_pins);
-
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
-    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
     digit_display_set_digit(digit_display, counter);
-
-    if (digitalRead(BUTTON_PIN) == LOW) {
-        if (!button_pressed) {
-            button_pressed = true;
-            digitalWrite(LED_PIN, HIGH);
-
-            if (counter == 15) {
-                counter = 0;
-            } else {
-                counter++;
-            }
-        }
+    if (counter == 15) {
+        counter = 0;
+    } else {
+        counter++;
     }
-    else {
-        button_pressed = false;
-        digitalWrite(LED_PIN, LOW);
-    }
+    delay(250);
 }
