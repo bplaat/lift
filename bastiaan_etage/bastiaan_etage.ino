b868c401a14ba2ca49d2f2f03ba4f83eb2d980bc
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-09-26 14:54:09 +0200

    Add up and down button logic

--- bastiaan_etage/bastiaan_etage.ino
+++ bastiaan_etage/bastiaan_etage.ino
@@ -5,23 +5,44 @@
 
 #include "digit_display.h"
 
-uint8_t counter = 0;
-
 DigitDisplay *digit_display;
 uint8_t digit_display_pins[7] = { 2, 3, 4, 5, 6, 7, 8 };
+
+#define UP_BUTTON_PIN 9
+bool upButtonDown = false;
+
+#define DOWN_BUTTON_PIN 10
+bool downButtonDown = false;
+
+#define LED_PIN 11
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Bastiaan's Etage");
     digit_display = digit_display_new(digit_display_pins);
+    pinMode(UP_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(DOWN_BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    digit_display_set_digit(digit_display, counter);
-    if (counter == 15) {
-        counter = 0;
-    } else {
-        counter++;
+    int upButtonState = digitalRead(UP_BUTTON_PIN);
+    if (upButtonState == LOW && !upButtonDown) {
+        upButtonDown = true;
+        digit_display_set_digit(digit_display, 0xa);
+        digitalWrite(LED_PIN, HIGH);
     }
-    delay(250);
+    if (upButtonState == HIGH) {
+        upButtonDown = false;
+    }
+
+    int downButtonState = digitalRead(DOWN_BUTTON_PIN);
+    if (downButtonState == LOW && !downButtonDown) {
+        downButtonDown = true;
+        digit_display_set_digit(digit_display, 0xb);
+        digitalWrite(LED_PIN, LOW);
+    }
+    if (downButtonState == HIGH) {
+        downButtonDown = false;
+    }
 }
