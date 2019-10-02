6223fcf7001586e71cb8f8b70e52fe1b271a9189
Jaco0987752 <0987752@hr.nl>
2019-10-02 14:04:42 +0200

    implemending the communication

--- jaco_etage/communication.h
+++ jaco_etage/communication.h
@@ -1,33 +1,44 @@
 //This code is written by Jaco de Jong
+#pragma once
+
 #include <Wire.h>
 #include "addresses.h"
 #include <Arduino.h>
 
-#pragma once
-
 #define NO_MOVING 0
 #define DIRECTION_DOWN 1
 #define DIRECTION_UP 2
+
 #define NO_STOP_NEEDED 0
 #define STOP_FOR_DOWN 1
 #define STOP_FOR_UP 2
+
+#define NO_STOP_ACCEPTED 0
+#define STOP_FOR_DOWN_ACCEPTED 1
+#define STOP_FOR_UP_ACCEPTED 2
+#define LiFT_IS_ARRIVED 3
+
 #define LIFT_IS_HERE 1
 #define LIFT_IS_NOT_HERE 0
-#define STOP_ACCEPTED 1
-#define NO_STOP_ACCEPTED 0
 
 uint8_t send_is_lift_here = LIFT_IS_NOT_HERE;
 uint8_t send_stop = NO_STOP_NEEDED;
 
 uint8_t recieved_floor = 0;
 uint8_t recieved_direction = NO_MOVING;
-uint8_t recieved_is_requested_stop_accepted = NO_STOP_ACCEPTED;
+uint8_t recieved_stop_accepted = NO_STOP_ACCEPTED;
 
 void receiveEvent()
 {
+    recieved_floor = Wire.read();
+    recieved_direction = Wire.read();
+    recieved_is_requested_stop_accepted = Wire.read();
 }
 void requestEvent()
 {
+    Wire.write(1);
+    Wire.write(send_is_lift_here);
+    Wire.write(send_stop);
 }
 
 void setup_I2C()
