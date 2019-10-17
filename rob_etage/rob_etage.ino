1056251f814532339253353bdef4af5373b7ef89
Rob Wolters <0993362@hr.nl>
2019-10-17 15:30:56 +0200

    Added start I2C

--- rob_etage/rob_etage.ino
+++ rob_etage/rob_etage.ino
@@ -1,19 +1,35 @@
-#include <wire.h>
+#include <Wire.h>
 
-int led = 9;
-int reed_switch = 13;
-int button_led = 6;
-int button_led2 = 5;
-int button_up = 7;
-int button_down = 8;
-const int latchPin = 12;  //Pin connected to ST_CP of 74HC595
-const int clockPin = 10;  //Pin connected to SH_CP of 74HC595 
-const int dataPin = 11;   //Pin connected to DS of 74HC595 
-int dat_array[7] = {252, 12, 182, 158, 78, 218, 250};
+#define led  9
+#define reed_switch  13
+#define button_led  6
+#define button_led2  5
+#define button_up  7
+#define button_down  8
+#define latchPin  12  //Pin connected to ST_CP of 74HC595
+#define clockPin  10  //Pin connected to SH_CP of 74HC595 
+#define dataPin  11   //Pin connected to DS of 74HC595 
+
+
+int dat_array[7] = {
+  B11111100,  // cijfer 0
+  B00001100,  // cijfer 1
+  B10110110,  // cijfer 2
+  B10011110,  // cijfer 3
+  B01001110,  // cijfer 4
+  B11011010,  // cijfer 5
+  B11111010}; // cijfer 6
+  
 bool travelling = false;
+const int ROB_ETAGE = 5;
 
 void setup ()
 {
+  Wire.begin(ROB_ETAGE);
+  Wire.onReceive(receive_event);
+  Wire.onRequest(request_event);
+  Serial.println("hetwerkt");
+  
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(reed_switch, INPUT_PULLUP);
@@ -23,7 +39,7 @@ void setup ()
   pinMode(button_led, OUTPUT);
   pinMode(button_led2, OUTPUT);
   pinMode(button_up, INPUT);
-  pinMode(button_down, INPUT_PULLUP);
+  pinMode(button_down, INPUT);
   
 }
 
@@ -33,7 +49,16 @@ void writeDigit(int i)
     shiftOut (dataPin, clockPin, MSBFIRST, i); // send data
     digitalWrite (latchPin, HIGH ); // latchPin high to save the data
  }
- 
+
+ void receive_event(){
+    receive_etage = Wire.read();
+    receive_direction = Wire.read();
+    receive_stop = Wire.read();
+ }
+
+ void request_event(){
+    
+ }
 void loop()
 {
   if (digitalRead(reed_switch) == LOW) // als er iets voor de sensor staat
@@ -49,11 +74,11 @@ void loop()
     digitalWrite(button_led, HIGH);
     writeDigit(dat_array[5]);
   }
-  else if (digitalRead(button_down) == LOW) // als de 2e knop ingedrukt word
+  else if (digitalRead(button_down) == HIGH) // als de 2e knop ingedrukt word
   { 
     travelling = true;
     digitalWrite(button_led2, HIGH);
-    writeDigit(dat_array[4];
+    writeDigit(dat_array[4]);
   }
   else
   {
