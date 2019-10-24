cc2b40315f796f4d734507c21acd534de1d8601c
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-10-24 09:03:55 +0200

    Add the first master stop code convert from JavaScript to C

--- machine_etage/machine_etage.ino
+++ machine_etage/machine_etage.ino
@@ -5,12 +5,31 @@
 // Include the wire library for I2C
 #include <Wire.h>
 
+// Include the keypad library for the input keypad
+#include <Keypad.h>
+
 // Enable the DEBUG flag
 #define DEBUG
 
+// Keypad vars
+#define KEYPAD_ROWS 4
+#define KEYPAD_COLUMNS 4
+
+char keypad_keys[KEYPAD_ROWS][KEYPAD_COLUMNS]= {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
+
+uint8_t keypad_row_pins[KEYPAD_ROWS] = { 2, 3, 4, 5 };
+uint8_t keypad_column_pins[KEYPAD_COLUMNS] = { 6, 7, 8, 9 };
+
+Keypad keypad = Keypad(makeKeymap(keypad_keys), keypad_row_pins, keypad_column_pins, KEYPAD_ROWS, KEYPAD_COLUMNS);
+
 // Global lift variables
 #define LIFT_ETAGES_COUNT 5
-#define PROTOCOL_RETURN_MESSAGE_LENGTH 3
+#define PROTOCOL_REQUEST_MESSAGE_LENGTH 3
 
 #define UP 1
 #define DOWN -1
@@ -22,7 +41,8 @@
 uint8_t lift_etage = 0;
 uint8_t lift_state = LIFT_STATE_STILL;
 
-// Stops code
+// Lift input led pins
+#define LED_PIN 10
 
 // The stop struct
 typedef struct Stop {
@@ -33,21 +53,111 @@ typedef struct Stop {
   uint8_t reported;
 } Stop;
 
-Stop *stop_new(uint8_t etage, int8_t direction, uint8_t begin, uint8_t end, uint8_t reported) {
-  Stop *stop = (Stop *)malloc(sizeof(Stop));
-  stop->etage = etage;
-  stop->direction = direction;
-  stop->begin = begin;
-  stop->end = end;
-  stop->reported = reported;
-  return stop;
+// The stops array
+#define STOPS_MAX_LENGTH 20
+Stop *stops[STOPS_MAX_LENGTH] = { 0 };
+uint8_t stops_length = 0;
+
+// Function that removes the first stop from the stops array
+void stops_shift() {
+  free(stops[0]);
+  for (uint8_t i = 1; i < stops_length; i++) {
+    stops[i - 1] = stops[i];
+  }
+  stops_length--;
 }
 
-void stop_free(Stop *stop) {
-  free(stop);
+// Vars for the end stop wait
+#define WAIT_TIME 1500
+#define WAIT_STATE_OFF 0
+#define WAIT_STATE_ON 1
+uint8_t wait_state = 0;
+uint32_t wait_time = 0;
+
+// Stops compare algorithm
+// -1 = stop is earlier
+// 0 = don't move stop
+// 1 = stop is later
+int16_t stops_compare(Stop *a, Stop *b) {
+  if (a->direction != b->direction) {
+    if (a->end && !b->end) {
+      return -1;
+    }
+    if (!a->end && b->end) {
+      return 1;
+    }
+  }
+  if (a->direction == stops[0]->direction && b->direction != stops[0]->direction) {
+    return -1;
+  }
+  if (a->direction != stops[0]->direction && b->direction == stops[0]->direction) {
+    return 1;
+  }
+  if (abs(lift_etage - a->etage) < abs(lift_etage - b->etage)) {
+    return -1;
+  }
+  if (abs(lift_etage - a->etage) > abs(lift_etage - b->etage)) {
+    return 1;
+  }
+  return 0;
 }
 
-// Main code
+// Function that handles a stop move end
+void handle_stops_end () {
+  lift_state = LIFT_STATE_STILL;
+  if (stops_length > 0) {
+    if (!stops[0]->begin && stops[0]->end) {
+      stops_shift();
+      wait_state = WAIT_STATE_ON;
+      wait_time = millis();
+    } else {
+      stops_shift();
+      lift_state = LIFT_STATE_WAITING;
+    }
+  }
+}
+
+// Function that moves the lift cabine to the first stop etage
+void goto_first_stop() {
+  // lift cabine move to stops[0]->etage
+}
+
+// Function that updates the stops array with a new stop or edits a stop
+void update_stops(uint8_t etage, int8_t direction, uint8_t end) {
+  if (stops_length >= STOPS_MAX_LENGTH) {
+    #ifdef DEBUG
+      Serial.println("ERROR: Max stops length!");
+    #endif
+    return;
+  }
+
+  uint8_t already_stop = 0;
+  for (uint8_t i = 0; i < stops_length; i++) {
+    if (stops[i]->etage == etage) {
+      already_stop = 1;
+      if (!end) {
+        stops[i]->direction = direction;
+        stops[i]->begin = 1;
+        stops[i]->reported = 0;
+      } else {
+        stops[i]->end = 1;
+      }
+      break;
+    }
+  }
+
+  if (!already_stop) {
+    Stop *stop = (Stop *)malloc(sizeof(Stop));
+    stop->etage = etage;
+    stop->direction = direction;
+    stop->begin = !end;
+    stop->end = end;
+    stop->reported = end;
+    stops[stops_length++] = stop;
+  }
+
+  qsort(stops, stops_length, sizeof(Stop *), stops_compare);
+}
 
 void setup() {
   // When in debug mode is enabled init the serial communication
@@ -61,6 +171,27 @@ void setup() {
 }
 
 void loop() {
+  // Check of the stop end state is waiting: wait and then go to the next best stop
+  if (wait_state == WAIT_STATE_ON && millis() - wait_time > WAIT_TIME) {
+    wait_state = WAIT_STATE_OFF;
+    if (stops_length > 0) {
+      qsort(stops, stops_length, sizeof(Stop *), stops_compare);
+      goto_first_stop();
+    }
+  }
+
+  // Check off the lift state is waiting
+  digitalWrite(LED_PIN, lift_state == LIFT_STATE_WAITING);
+
+  // If lift state is waiting get pressed key check if it's a good key and add stop
+  if (lift_state == LIFT_STATE_WAITING) {
+    uint8_t etage = keypad.getKey() - '0';
+    if (etage >= 0 && etage <= LIFT_ETAGES_COUNT && etage != lift_etage) {
+      update_stops(etage, etage > lift_etage ? UP : DOWN, true);
+      goto_first_stop();
+    }
+  }
+
   // Print the information that the master is going to send to all the slaves
   #ifdef DEBUG
     Serial.print("<- lift_etage = ");
@@ -70,21 +201,21 @@ void loop() {
   #endif
 
   // Loop over all the etages and send and receive information
-  for (uint8_t address = 1; address <= LIFT_ETAGES_COUNT; address++) {
+  for (uint8_t etage = 1; etage <= LIFT_ETAGES_COUNT; etage++) {
     // Send the information conform protocol
-    Wire.beginTransmission(address);
+    Wire.beginTransmission(etage);
     Wire.write(lift_etage);
     Wire.write(lift_state);
     Wire.write(0);
     Wire.endTransmission();
 
     // Request other information conform protocol
-    if (Wire.requestFrom(address, PROTOCOL_RETURN_MESSAGE_LENGTH) == PROTOCOL_RETURN_MESSAGE_LENGTH) {
+    if (Wire.requestFrom(etage, PROTOCOL_REQUEST_MESSAGE_LENGTH) == PROTOCOL_REQUEST_MESSAGE_LENGTH) {
       // Read ping value and ignore
       uint8_t ping = Wire.read();
       #ifdef DEBUG
         Serial.print("-> ping[");
-        Serial.print(address);
+        Serial.print(etage);
         Serial.print("] = ");
         Serial.println(ping);
       #endif
@@ -93,36 +224,39 @@ void loop() {
       uint8_t lift_is_here = Wire.read();
       #ifdef DEBUG
         Serial.print("-> lift_is_here[");
-        Serial.print(address);
+        Serial.print(etage);
         Serial.print("] = ");
         Serial.println(lift_is_here);
       #endif
 
       // If lift is here then update lift etage
       if (lift_is_here == 1) {
-        lift_etage = address;
+        lift_etage = etage;
       }
 
       // Read request lift stop value
       int8_t lift_request_stop = Wire.read();
       #ifdef DEBUG
         Serial.print("-> lift_request_stop[");
-        Serial.print(address);
+        Serial.print(etage);
         Serial.print("] = ");
         Serial.println(lift_request_stop);
       #endif
 
       // Handle request stop
-      if (lift_request_stop == UP || lift_request_stop == DOWN) {
-
+      if (lift_request_stop != 0) {
+          update_stops(etage, lift_request_stop, 0);
+          if (stops[0]->etage == etage && lift_state != LIFT_STATE_WAITING) {
+            goto_first_stop();
+          }
       }
     }
 
     // If slave etage didn't respond with the right amount of bytes then this exception will handle it
     #ifdef DEBUG
       else {
-        Serial.print("Etage ");
-        Serial.print(address);
+        Serial.print("ERROR: Etage ");
+        Serial.print(etage);
         Serial.println(" didn't respond the right amount of bytes!");
       }
     #endif
