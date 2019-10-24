5a7d039b6f4f73cadae8642457fd05afd10a7422
Jaco0987752 <0987752@hr.nl>
2019-10-24 10:18:29 +0200

    some bug fixes

--- jaco_etage/communication.h
+++ jaco_etage/communication.h
@@ -8,13 +8,12 @@
 
 #define NO_MOVING 0
 #define MOVING 1
-#define STOPPED_HERE 2
+#define WAITING 2
 
 #define NO_STOP_NEEDED 0
 #define STOP_FOR_DOWN 1
 #define STOP_FOR_UP 2
 
-#define NO_STOP_ACCEPTED 0
 #define STOP_FOR_DOWN_ACCEPTED -1
 #define STOP_FOR_UP_ACCEPTED 1
 
@@ -24,14 +23,24 @@ uint8_t send_is_lift_here = 0;
 uint8_t send_stop = 0;
 
 uint8_t recieved_floor = 1;
-uint8_t recieved_action = MOVING;                      //NO_MOVING;
-uint8_t recieved_stop_accepted = STOP_FOR_UP_ACCEPTED; //NO_STOP_ACCEPTED;
+uint8_t recieved_action = NO_MOVING;
+bool recieved_stop_accepted_for_up = 0; //STOP_FOR_UP_ACCEPTED; //NO_STOP_ACCEPTED;
+bool recieved_stop_accepted_for_down = 0;
 
 void receiveEvent()
 {
     recieved_floor = Wire.read();
     recieved_action = Wire.read();
-    recieved_stop_accepted = Wire.read();
+    int8_t recieved_stop_accepted = Wire.read();
+    if (recieved_stop_accepted == STOP_FOR_DOWN_ACCEPTED)
+    {
+        recieved_stop_accepted_for_down = HIGH;
+    }
+    if (recieved_stop_accepted == STOP_FOR_UP_ACCEPTED)
+    {
+        recieved_stop_accepted_for_up = HIGH;
+        Serial.println("checking in comm");
+    }
 }
 void requestEvent()
 {
@@ -45,15 +54,4 @@ void setup_I2C()
     Wire.begin(JACO_ETAGE);
     Wire.onReceive(receiveEvent);
     Wire.onRequest(requestEvent);
-}
-
-void receiveEventForAnsiCharacters()
-{
-    while (1 < Wire.available())
-    {
-        char c = Wire.read();
-        Serial.print(c);
-    }
-    char recieved_byte = Wire.read();
-    Serial.print(recieved_byte);
 }
